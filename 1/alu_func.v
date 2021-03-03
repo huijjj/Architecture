cb0b48ac2a27ae92910f69149c2ffb23031e519e
정희종 <huijongjeong@jeonghuijongs-Mac-mini.local>
2021-03-03 17:08:56 +0900

    Uploading to github for collaboration

---
+++ 1/alu_func.v
@@ -0,0 +1,18 @@
+// FuncCodes
+`define FUNC_ADD    4'b0000
+`define FUNC_SUB    4'b0001
+`define FUNC_ID     4'b0010
+`define FUNC_NOT    4'b0011
+`define FUNC_AND    4'b0100
+`define FUNC_OR     4'b0101
+`define FUNC_NAND   4'b0110
+`define FUNC_NOR    4'b0111
+`define FUNC_XOR    4'b1000
+`define FUNC_XNOR   4'b1001
+`define FUNC_LLS    4'b1010
+`define FUNC_LRS    4'b1011
+`define FUNC_ALS    4'b1100
+`define FUNC_ARS    4'b1101
+`define FUNC_TCP    4'b1110
+`define FUNC_ZERO   4'b1111
+
