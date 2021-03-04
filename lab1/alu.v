3cf3c7b779bf476dd780c5adf7d0de0a08911a29
huijjj <hui0213@postech.ac.kr>
2021-03-04 19:56:53 +0900

    save for debugging

--- lab1/alu.v
+++ lab1/alu.v
@@ -10,7 +10,6 @@ module ALU #(parameter data_width = 16) (
 /*
 	YOUR VARIABLE DECLARATION...
 */
-`include "alu_func.v"
 
 initial begin
 	C = 0;
@@ -22,27 +21,6 @@ end
 /*
 	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
 */
-
-	case(FuncCode)
-		`FUNC_ADD:
-		`FUNC_SUB:
-		`FUNC_ID     4'b001
-		`FUNC_NOT    4'b0011
-`define FUNC_AND    4'b0100
-`define FUNC_OR     4'b0101
-`define FUNC_NAND   4'b0110
-`define FUNC_NOR    4'b0111
-`define FUNC_XOR    4'b1000
-`define FUNC_XNOR   4'b1001
-`define FUNC_LLS    4'b1010
-`define FUNC_LRS    4'b1011
-`define FUNC_ALS    4'b1100
-`define FUNC_ARS    4'b1101
-`define FUNC_TCP    4'b1110
-`define FUNC_ZERO   4'b1111
-
-
-
 
 endmodule
 
---
+++ lab1/alu_modules.v
@@ -0,0 +1,68 @@
+`ifndef ALU_MODULES_V
+`define ALU_MODULES_V
+
+module add_sub_module #(parameter data_width = 16) (
+	input [data_width - 1 : 0] _A,
+	input [data_width - 1 : 0] _B,
+	input func,
+	output reg [data_width - 1 : 0] _C,
+	output reg _O
+);
+	reg [data_width - 1 : 0] temp;
+
+	always @(*) begin
+		if(func) // SUB
+			temp = -_B;
+		else // ADD
+			temp = _B;
+
+		_C = _A + temp;
+		if(_A[data_width - 1] & temp[data_width - 1])
+			_O = _A[data_width - 1] ^ _C[data_width - 1];
+		else
+			_O = 1'b0;
+	end
+endmodule
+
+module identity_not_module #(parameter data_width = 16) (
+	input [data_width - 1 : 0]_A,
+	input func,
+	output reg [data_width - 1 : 0] _C
+);
+	always @(*) begin
+		if(func) // NOT
+			_C <= ~_A;
+		else // IDENTITY
+			_C <= _A;
+	end
+endmodule
+
+module and_or_module #(parameter data_width = 16) (
+	input [data_width - 1 : 0]_A,
+	input [data_width - 1 : 0]_B,
+	input func,
+	output reg [data_width - 1 : 0] _C
+);
+	always @(*) begin
+		if(func) // OR
+			_C <= _A|_B;
+		else // AND
+			_C <= _A&_B; 
+	end	
+endmodule
+
+module nand_nor_module #(parameter data_width = 16) (
+	input [data_width - 1 : 0]_A,
+	input [data_width - 1 : 0]_B,
+	input func,
+	output reg [data_width - 1 : 0] _C
+);
+	always @() begin
+		if(func) // NOR
+			_C <= ~(_A|_B);
+		else // NAND	
+			_C <= ~(_A&_B);	
+	end
+endmodule
+
+`endif
