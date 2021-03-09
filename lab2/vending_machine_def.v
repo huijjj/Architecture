039d7aa1dc82c7198b45bfcff39cf307ac6467bb
huijjj <hui0213@postech.ac.kr>
2021-03-10 08:49:24 +0900

    fix: fixed files

---
+++ lab2/vending_machine_def.v
@@ -0,0 +1,14 @@
+// Title         : vending_machine_def.v
+// Author      : Jae-Eon Jo (Jojaeeon@postech.ac.kr) 
+//					Dongup Kwon (nankdu7@postech.ac.kr)
+
+// Macro constants (prefix k & CamelCase)
+`define kTotalBits 31
+  
+`define kItemBits 8
+`define kNumItems 4
+
+`define kCoinBits 8
+`define kNumCoins 3
+
+`define kWaitTime 10
