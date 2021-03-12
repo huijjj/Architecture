939931f7d6a07ef6724cca2b7803566e2d363971
huijjj <hui0213@postech.ac.kr>
2021-03-12 16:46:38 +0900

    add: check_time_and_coin module

--- lab2/check_time_and_coin.v
+++ lab2/check_time_and_coin.v
@@ -6,31 +6,53 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,wait_time,o_re
 	input clk;
 	input reset_n;
 	input [`kNumCoins-1:0] i_input_coin;
-	input [`kNumItems-1:0]	i_select_item;
+	input [`kNumItems-1:0] i_select_item;
+
 	output reg  [`kNumCoins-1:0] o_return_coin;
 	output reg [31:0] wait_time;
+
+
+	reg [31 : 0] _wait_time;
+	reg [`kNumCoins-1 : 0] return_coin;
 
 	// initiate values
 	initial begin
 		// TODO: initiate values
+		_wait_time = `kWaitTime;
+		return_coin = 0;
 	end
 
 
 	// update coin return time
 	always @(i_input_coin, i_select_item) begin
-		// TODO: update coin return time
+		// TODO: update coin return time		
+		_wait_time = `kWaitTime;
 	end
 
 	always @(*) begin
 		// TODO: o_return_coin
+		wait_time = _wait_time;
+		return_coin = (i_input_coin & return_coin);
 	end
+
+
 
 	always @(posedge clk ) begin
 		if (!reset_n) begin
 		// TODO: reset all states.
+			_wait_time <= `kWaitTime;
+			return_coin <= 0;
+			wait_time <= `kWaitTime;
+			o_return_coin <= 0;
 		end
+
 		else begin
 		// TODO: update all states.
+			_wait_time <= (_wait_time - 1);
+			o_return_coin <= return_coin;
 		end
 	end
 endmodule 
+
+// i_input_coin 혹은 i_select_item이 입력되면 time을 초기화함
+// 
