f036bbc019a5e944f3b1782606cf9a75dd78b996
huijjj <hui0213@postech.ac.kr>
2021-03-12 17:59:38 +0900

    fix: add i_trigger_return to check_time_and_coin module

--- lab2/check_time_and_coin.v
+++ lab2/check_time_and_coin.v
@@ -7,19 +7,20 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,wait_time,o_re
 	input reset_n;
 	input [`kNumCoins-1:0] i_input_coin;
 	input [`kNumItems-1:0] i_select_item;
+	input i_trigger_return;
 
 	output reg  [`kNumCoins-1:0] o_return_coin;
 	output reg [31:0] wait_time;
 
 
 	reg [31 : 0] _wait_time;
-	reg [`kNumCoins-1 : 0] return_coin;
+	reg [30 : 0] remain;
 
 	// initiate values
 	initial begin
 		// TODO: initiate values
 		_wait_time = `kWaitTime;
-		return_coin = 0;
+		remain = 0;
 	end
 
 
@@ -27,29 +28,111 @@ module check_time_and_coin(i_input_coin,i_select_item,clk,reset_n,wait_time,o_re
 	always @(i_input_coin, i_select_item) begin
 		// TODO: update coin return time		
 		_wait_time = `kWaitTime;
+
+		if(i_input_coin[0]) begin
+			remain += 100;
+		end
+
+		if(i_input_coin[1]) begin
+			remain += 500;
+		end
+		
+		if(i_input_coin[2]) begin
+			remain += 1000;
+		end
+
+		if(i_select_item[0]) begin
+			if(remain >= 400) begin
+				remain -= 400;
+			end
+		end
+
+		if(i_select_item[1]) begin
+			if(remain >= 500) begin
+				remain -= 500;
+			end
+		end
+
+		if(i_select_item[2]) begin
+			if(remain >= 1000) begin
+				remain -= 1000;
+			end
+		end
+
+		if(i_select_item[3]) begin
+			if(remain >= 2000) begin
+				remain -= 2000;
+			end
+		end
 	end
+
 
 	always @(*) begin
 		// TODO: o_return_coin
+		if(i_trigger_return)
+		{
+			_wait_time = 0;
+		}
 		wait_time = _wait_time;
-		return_coin = (i_input_coin & return_coin);
+		if(!_wait_time)
+		{
+			if(remain >= 1600)
+			{
+				remain -= 1600;
+				o_return_coin = 3'b111;
+			}
+			else if(remain >= 1500)
+			{
+				remain -= 1500;
+				o_return_coin = 3'b110;
+			}
+			else if(remain >= 1100)
+			{
+				remain -= 1100;
+				o_return_coin = 3'b101;
+			}
+			else if(remain >= 1000)
+			{
+				remain -= 1000;
+				o_return_coin = 3'b100;
+			}
+			else if(remain >= 600)
+			{
+				remain -= 600;
+				o_return_coin = 3'b011;
+			}
+			else if(remain >= 500)
+			{
+				remain -= 500;
+				o_return_coin = 3'b010;
+			}
+			else if(remain >= 100)
+			{
+				remain -= 100;
+				o_return_coin = 3'b001;
+			}
+			else
+			{
+				o_return_coin = 3'b000;
+			}	
+		}
 	end
-
-
 
 	always @(posedge clk ) begin
 		if (!reset_n) begin
 		// TODO: reset all states.
 			_wait_time <= `kWaitTime;
-			return_coin <= 0;
-			wait_time <= `kWaitTime;
-			o_return_coin <= 0;
+			remain <= 0;	
 		end
 
 		else begin
 		// TODO: update all states.
-			_wait_time <= (_wait_time - 1);
-			o_return_coin <= return_coin;
+			if(_wait_time) begin
+				_wait_time <= (_wait_time - 1);
+			end
+			else begin
+				_wait_time <= _wait_time;
+			end
 		end
 	end
 endmodule 
