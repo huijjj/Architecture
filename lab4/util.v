61ce1716080b3d46212f593cf3534473e925cd94
huijjj <hui0213@postech.ac.kr>
2021-04-02 17:36:47 +0900

    add: register files

--- lab4/util.v
+++ lab4/util.v
@@ -1,14 +1,14 @@
 module mux4_1 (sel, i1, i2, i3, i4, o);
    input [1:0] sel;
-   input [15:0] i1, i2, i3, i4;
+   input [15:0] i0, i1, i2, i3;
    output reg [15:0] o;
 
    always @ (*) begin
       case (sel)
-         0: o = i1;
-         1: o = i2;
-         2: o = i3;
-         3: o = i4;
+         0: o = i0;
+         1: o = i1;
+         2: o = i2;
+         3: o = i3;
       endcase
    end
 
@@ -17,13 +17,13 @@ endmodule
 
 module mux2_1 (sel, i1, i2, o);
    input [1:0] sel;
-   input [15:0] i1, i2;
+   input [15:0] i0, i1;
    output reg [15:0] o;
 
    always @ (*) begin
       case (sel)
-         0: o = i1;
-         1: o = i2;
+         0: o = i0;
+         1: o = i1;
       endcase
    end
 
