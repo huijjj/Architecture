9c88e3141ad1043424aa8a7c06e1b9e28791bde1
이경민 <rudals@useumeong.local>
2021-04-15 21:45:55 +0900

    control unit

--- lab4/alu.v
+++ lab4/alu.v
@@ -5,12 +5,107 @@
 module alu (A, B, func_code, branch_type, C, overflow_flag, bcond);
    input [`NumBits-1:0] A; //input data A
    input [`NumBits-1:0] B; //input data B
-   input [3:0] func_code; //function code for the operation
-   input [1:0] branch_type; //branch type for bne, beq, bgz, blz
+   input [2:0] func_code; //function code for the operation
+   input [1:0] branch_type; //branch type for bne, beq, bgz, blz and 이단아
    output reg [`NumBits-1:0] C; //output data C
    output reg overflow_flag; 
    output reg bcond; //1 if branch condition met, else 0
 
    //TODO: implement ALU
-   
+   initial begin
+      C = 0;
+      overflow_flag = 0;
+      bcond = 0;
+   end
+
+   always @(*) begin
+      case(func_code)
+         `FUNC_ADD: begin
+            C = A + B;
+            case(branch_type)
+               2'b10: begin // bgz
+				   	if(A > 0) begin
+				   		bcond = 1;
+				   	end
+				   	else begin
+				   		bcond = 0;
+				   	end
+				   end
+				   2'b11: begin // blz
+				   	if(A < 0) begin
+				   		bcond = 1;
+				   	end
+				   	else begin
+				   		bcond = 0;
+				   	end
+				   end
+               default: begin
+                  bcond = 0;
+                  //todo overflow
+               end
+            endcase
+         end
+			`FUNC_SUB: begin
+				C = A - B;
+				case(branch_type)
+					2'b00: begin // bne
+						if(C == 0) begin
+							bcond = 0;
+						end
+						else begin
+							bcond = 1;
+						end
+					end
+					2'b01: begin // beq
+						if(C == 0) begin
+							bcond = 1;
+						end
+						else begin
+							bcond = 0;
+						end
+					end
+					default: begin
+						bcond = 0;
+                  //todo overflow
+					end
+				endcase
+			end
+			`FUNC_AND: begin
+				C = A & B;
+				bcond = 0;
+			end
+			`FUNC_ORR: begin
+				C = A | B;
+				bcond = 0;
+			end
+			`FUNC_NOT: begin
+				C = ~A;
+				bcond = 0;
+			end
+			`FUNC_TCP: begin
+				C = ~A + 1;
+				bcond = 0;
+			end
+			`FUNC_SHL: begin
+				case(branch_type)
+					2'b01: begin
+						C = B << 8;
+					end
+					2'b00: begin
+						C = A << 1;
+					end
+               default: begin
+               end
+				endcase
+				bcond = 0;
+			end
+			`FUNC_SHR: begin
+				C = A >> 1;
+				C[`NumBits-1] = A[`NumBits-1];
+				bcond = 0;
+			end
+         default:
+         end
+      endcase
+   end
 endmodule
--- lab4/control_unit.v
+++ lab4/control_unit.v
@@ -1,18 +1,684 @@
 `include "opcodes.v"
+`defind IF 3'd0;
+`defind EX_1 3'd1;
+`defind EX_2 3'd2;
+`defind MEM 3'd3;
+`defind WB 3'd4;
 
-module control_unit(opcode, func_code, clk, pc_write_cond, pc_write, i_or_d, mem_read, mem_to_reg, mem_write, ir_write, pc_to_reg, pc_src, halt, wwd, new_inst, reg_write, alu_src_A, alu_src_B, alu_op);
+
+module control_unit(opcode, func_code, clk, mem_wirte, mem_read, reg_write, reg_dst, pc_to_reg, mem_to_reg, alu_src_A, alu_src_B, pc_store, branch_dst_store, branch, jal, jalr, PVSupdate ,alu_op, halt, wwd);
   input [3:0] opcode;
   input [5:0] func_code;
   input clk;
 
-  output reg pc_write_cond, pc_write, i_or_d, mem_read, mem_to_reg, mem_write, ir_write, pc_src;
-  //additional control signals. pc_to_reg: to support JAL, JRL. halt: to support HLT. wwd: to support WWD. new_inst: new instruction start
-  output reg pc_to_reg, halt, wwd, new_inst;
-  output reg [1:0] reg_write, alu_src_A, alu_src_B;
-  output reg alu_op;
+  //얘네들은 어떻게 해야할지 좀 더 고민해보기ㅕㅓ
+  //output reg alu_op;
+  //output reg halt, wwd;
 
+  output reg mem_write, mem_read;
+  output reg reg_write;
+  output reg reg_dst, pc_to_reg, mem_to_reg;
+  output reg alu_src_A, alu_src_B;
+  output reg pc_store, branch_dst_store;
+  output reg branch, jal, jalr; 
+  output reg PVSupdate;
 
-   //TODO: implement control unit
+  reg [2:0] cur_state;
+  reg [2:0] next_state;
 
+  initial begin
+    mem_write = 0;
+    mem_read = 0;
+    reg_write = 0;
+    reg_dst = 0;
+    pc_to_reg = 0;
+    mem_to_reg = 0;
+    alu_src_A = 0;
+    alu_src_B = 0;
+    pc_store = 0;
+    branch_dst_store = 0;
+    branch = 0;
+    jal = 0;
+    jalr = 0;
+    PVSupdate = 0;
 
+    cur_state = 0;
+    next_state = 0;
+  end
+  
+  always @(posedge clk) begin
+    cur_state <= next_state;
+  end
+
+  always @(*) begin
+    case(opcode)
+      `ALU_OP: begin
+        case(func_code)
+          `INST_FUNC_ADD,
+          `INST_FUNC_SUB,
+          `INST_FUNC_AND,
+          `INST_FUNC_ORR,
+          `INST_FUNC_NOT,
+          `INST_FUNC_TCP,
+          `INST_FUNC_SHL,
+          `INST_FUNC_SHR: begin
+            case(cur_state)
+              `IF: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 1;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 0;
+                next_state = 1;
+              end
+              `EX_1: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 1;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 0;
+                next_state = 4;  
+              end
+              `WB: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 1;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 1;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 1;
+                next_state = 0;
+              end
+              default: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 0;
+                next_state = 0;
+              end
+            endcase
+          end
+          `INST_FUNC_JPR: begin
+            case(cur_state)
+              `IF: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 1;
+                PVSupdate = 0;
+                next_state = 1;
+              end
+              `EX_1: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 1;
+                PVSupdate = 1;
+                next_state = 0;
+              end
+              default: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 0;
+                next_state = 0;
+              end
+            endcase
+          end
+          `INST_FUNC_JRL: begin
+            case(cur_state)
+              `IF: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 1;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 1;
+                PVSupdate = 0;
+                next_state = 4;
+              end
+              `WB: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 1;
+                reg_dst = 0;
+                pc_to_reg = 1;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 1;
+                PVSupdate = 1;
+                next_state = 0;
+              end
+              default: begin
+                mem_write = 0;
+                mem_read = 0;
+                reg_write = 0;
+                reg_dst = 0;
+                pc_to_reg = 0;
+                mem_to_reg = 0;
+                alu_src_A = 0;
+                alu_src_B = 0;
+                pc_store = 0;
+                branch_dst_store = 0;
+                branch = 0;
+                jal = 0;
+                jalr = 0;
+                PVSupdate = 0;
+                next_state = 0;
+              end
+            endcase
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0;
+          end
+        endcase
+      end
+      `ADI_OP,
+      `ORI_OP,
+      `LHI_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store =1;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 1;
+          end
+          `EX_1: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 4;  
+          end
+          `WB: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 1;
+            reg_dst = 1;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0;
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0;
+          end
+        endcase
+      end
+      `LWD_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 1;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 1;
+          end
+          `EX_1: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 3;
+          end
+          `MEM: begin
+            mem_write = 0;
+            mem_read = 1;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 4;  
+          end
+          `WB: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 1;
+            reg_dst = 1;
+            pc_to_reg = 0;
+            mem_to_reg = 1;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0;  
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 1;
+            reg_dst = 1;
+            pc_to_reg = 0;
+            mem_to_reg = 1;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 4; 
+          end
+      end
+      `SWD_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 1;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 1;
+          end
+          `EX_1: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 3; 
+          end
+          `MEM: begin
+            mem_write = 1;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0; 
+          end
+          default: begin
+            em_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0; 
+          end
+        endcase
+      end
+      `BNE_OP,
+      `BEQ_OP,
+      `BGZ_OP,
+      `BLZ_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 1;
+            branch_dst_store = 0;
+            branch = 1;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 1; 
+          end
+          `EX_1: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 1;
+            pc_store = 0;
+            branch_dst_store = 1;
+            branch = 1;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 2; 
+          end
+          `EX_2: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 1;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 1;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0; 
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0; 
+          end
+        endcase
+      `JMP_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 1;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 1; 
+          end
+          `EX_1: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 1;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0; 
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0; 
+          end
+        endcase
+      end
+      `JAL_OP: begin
+        case(cur_state)
+          `IF: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 1;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 1;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 4; 
+          end
+          `WB: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 1;
+            jalr = 0;
+            PVSupdate = 1;
+            next_state = 0; 
+          end
+          default: begin
+            mem_write = 0;
+            mem_read = 0;
+            reg_write = 0;
+            reg_dst = 0;
+            pc_to_reg = 0;
+            mem_to_reg = 0;
+            alu_src_A = 0;
+            alu_src_B = 0;
+            pc_store = 0;
+            branch_dst_store = 0;
+            branch = 0;
+            jal = 0;
+            jalr = 0;
+            PVSupdate = 0;
+            next_state = 0; 
+          end
+        endcase
+    endcase
+  end
 endmodule
