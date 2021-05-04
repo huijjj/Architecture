80b17ff617af1fc5b8b3bc927cb725fc247e9e20
huijjj <hui0213@postech.ac.kr>
2021-05-05 03:31:56 +0900

    add: alu, control_unit, hazrd_detection_unit, forwarding_unit

--- lab5/control_unit.v
+++ lab5/control_unit.v
@@ -4,18 +4,417 @@ module control_unit (opcode, func_code, clk, reset_n, pc_write_cond, pc_write, i
 
 	input [3:0] opcode;
 	input [5:0] func_code;
-	input clk;
+
 	input reset_n;
 	
+	output reg halt, reg_dst, pc_to_reg;
+	output reg alu_src, branch, JPR_or_JRL, JMP_or_JAL;
+	output reg mem_write, mem_read;
+	output reg mem_to_reg, wwd;
+	output reg [2:0] alu_op;
+	output reg [1:0] branch_type;
 
-	output reg pc_write_cond, pc_write, i_or_d, mem_read, mem_to_reg, mem_write, ir_write, pc_src;
-  	//additional control signals. pc_to_reg: to support JAL, JRL. halt: to support HLT. wwd: to support WWD. new_inst: new instruction start
-  	output reg pc_to_reg, halt, wwd, new_inst;
-  	output reg [1:0] reg_write, alu_src_A, alu_src_B;
-  	output reg alu_op;
+	initial begin
+		halt = 0;
+		reg_dst = 0;
+		pc_to_reg = 0;
+		alu_src = 0;
+		branch = 0;
+		JPR_or_JRL  = 0;
+		JMP_or_JAL = 0;
+		mem_write = 0;
+		mem_read = 0;
+		mem_to_reg = 0;
+		wwd = 0;
+		alu_op = 0;
+		branch_type = 0;
+	end
 
-	//TODO : implement control unit
+	always @(*) begin
+   		if(!reset_n) begin
+	    	halt = 0;
+			reg_dst = 0;
+			pc_to_reg = 0;
+			alu_src = 0;
+			branch = 0;
+			JPR_or_JRL  = 0;
+			JMP_or_JAL = 0;
+			mem_write = 0;
+			mem_read = 0;
+			mem_to_reg = 0;
+			wwd = 0;
+			alu_op = 0;
+			branch_type = 0;
+    	end
+  	end
 
-
-
+	always @(*) begin
+		case(opcode)
+			`ALU_OP: begin
+				case(func_code)
+					`INST_FUNC_ADD: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_ADD;
+						branch_type = 0;
+					end 
+					`INST_FUNC_SUB: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_SUB;
+						branch_type = 2'b11;
+					end 
+					`INST_FUNC_AND: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_AND;
+						branch_type = 0;
+					end 
+					`INST_FUNC_ORR: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_ORR;
+						branch_type = 0;
+					end 
+					`INST_FUNC_NOT: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_NOT;
+						branch_type = 0;
+					end 
+					`INST_FUNC_TCP: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_TCP;
+						branch_type = 0;
+					end 
+					`INST_FUNC_SHL: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_SHL;
+						branch_type = 0;
+					end 
+					`INST_FUNC_SHR: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = `FUNC_SHL;
+						branch_type = 0;
+					end 
+					`INST_FUNC_JPR: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 1;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = 0;
+						branch_type = 0;
+					end
+					`INST_FUNC_JRL: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 1;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 1;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = 0;
+						branch_type = 0;
+					end
+					`INST_FUNC_WWD: begin
+						halt = 0;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 1;
+						alu_op = 0;
+						branch_type = 0;
+					end
+					`INST_FUNC_HLT: begin
+						halt = 1;
+						reg_dst = 0;
+						pc_to_reg = 0;
+						alu_src = 0;
+						branch = 0;
+						JPR_or_JRL  = 0;
+						JMP_or_JAL = 0;
+						mem_write = 0;
+						mem_read = 0;
+						mem_to_reg = 0;
+						wwd = 0;
+						alu_op = 0;
+						branch_type = 0;
+						
+					end
+				endcase
+			end
+			`ADI_OP: begin
+				halt = 0;
+				reg_dst = 1;
+				pc_to_reg = 0;
+				alu_src = 1;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_ADD;
+				branch_type = 0;
+			end
+			`ORI_OP: begin
+				halt = 0;
+				reg_dst = 1;
+				pc_to_reg = 0;
+				alu_src = 1;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_ORR;
+				branch_type = 0;
+			end
+			`LHI_OP: begin
+				halt = 0;
+				reg_dst = 1;
+				pc_to_reg = 0;
+				alu_src = 1;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_SHL;
+				branch_type = 2'b01;
+			end
+			`LWD_OP: begin
+				halt = 0;
+				reg_dst = 1;
+				pc_to_reg = 0;
+				alu_src = 1;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 1;
+				mem_to_reg = 1;
+				wwd = 0;
+				alu_op = `FUNC_ADD;
+				branch_type = 0;
+			end
+			`SWD_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 1;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 1;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_ADD;
+				branch_type = 0;
+			end
+			`BNE_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 1;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_SUB;
+				branch_type = 2'b00;
+			end
+			`BEQ_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 1;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_SUB;
+				branch_type = 2'b01;
+			end
+			`BGZ_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 1;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_ADD;
+				branch_type = 2'b10;
+			end
+			`BLZ_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 1;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = `FUNC_ADD;
+				branch_type = 2'b11;
+			end
+			`JMP_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 1;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = 0;
+				branch_type = 0;
+			end
+			`JAL_OP: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 1;
+				alu_src = 0;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 1;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = 0;
+				branch_type = 0;
+			end
+			default: begin
+				halt = 0;
+				reg_dst = 0;
+				pc_to_reg = 0;
+				alu_src = 0;
+				branch = 0;
+				JPR_or_JRL  = 0;
+				JMP_or_JAL = 0;
+				mem_write = 0;
+				mem_read = 0;
+				mem_to_reg = 0;
+				wwd = 0;
+				alu_op = 0;
+				branch_type = 0;
+			end
+		endcase	
+	end
 endmodule
--- lab5/hazard.v
+++ lab5/hazard.v
@@ -8,6 +8,78 @@ module hazard_detect(IFID_IR, IDEX_rd, IDEX_M_mem_read, is_stall);
 
 	output is_stall;
 
-	//TODO: implement hazard detection unit
+	wire use_rs_inst;
+	use_rs check_use_rs(
+		.instruction(IFID_IR),
+		.use(use_rs_inst)
+	);
 
+	wire use_rt_inst;
+	use_rt check_use_rt(
+		.instruction(IFID_IR),
+		.use(use_rt_inst)
+	);
+
+	if(IDEX_M_mem_read) begin
+		if((use_rs_inst && (IFID_IR[11:10] == IDEX_rd)) || (use_rt_inst && (IFID_IR[9:8] == IDEX_rd))) begin
+			is_stall = 1;
+		end
+		else begin
+			is_stall = 0;
+		end
+	end
+	else begin
+		is_stall = 0;
+	end
+endmodule
+
+module use_rs(instruction, use);
+	input [`WORD_SIZE-1:0];
+	output use;
+
+	always @(*) begin
+		if(instruction[15:12] == `JMP_OP || instruction[15:12] = `JAL_OP || (instruction[15:12] == `HLT_OP && instruction[5:0] == `INST_FUNC_HLT)) begin
+			// JMP or JAL or HLT
+			use = 0;
+		end
+		else begin
+			use = 1;
+		end	
+	end
+endmodule
+
+module use_rt(instruction, use);
+	input [`WORD_SIZE-1:0];
+	output use;
+	
+	// ADD, SUB, AND, ORR, SWD, BXX -> use = 1;
+
+
+	always @(*) begin
+		case(instrcution[15:12])
+			`ALU_OP: begin
+				case(instruction[5:0])
+					`INST_FUNC_ADD,
+					`INST_FUNC_SUB,
+					`INST_FUNC_AND,
+					`INST_FUNC_ORR: begin
+						use = 1;
+					end
+					default: begin
+						use = 0;
+					end
+				endcase
+			end
+			`SWD_OP,  
+			`BNE_OP,
+			`BEQ_OP,
+			`BGZ_OP,
+			`BLZ_OP: begin
+				use = 1;
+			end
+			default: begin
+				use = 0;
+			end
+		endcase
+	end
 endmodule
